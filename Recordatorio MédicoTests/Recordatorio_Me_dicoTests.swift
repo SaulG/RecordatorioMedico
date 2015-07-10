a469db0f45f4a43a171fa0c356fdd491ce748fc6
Saul Gausin <saul.gausin@gmail.com>
2015-07-10 12:55:50 -0500

    Initial Commit

---
+++ Recordatorio MédicoTests/Recordatorio_Me_dicoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Recordatorio_Me_dicoTests.swift
+//  Recordatorio MédicoTests
+//
+//  Created by saul on 10/07/15.
+//  Copyright (c) 2015 SaulGausin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Recordatorio_Me_dicoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
